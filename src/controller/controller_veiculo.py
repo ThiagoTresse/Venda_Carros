33cd47f8906c936d0a8f1be96d5ea72eb0c14685
ThiagoTr <thiagotresse@hotmail.com>
2023-10-16 17:56:33 -0300

    controller veiculos em andamento...

--- src/controller/controller_veiculo.py
+++ src/controller/controller_veiculo.py
@@ -1,61 +1,86 @@
 from model.veiculos import Veiculo
 from conexion.oracle_queries import OracleQueries
 
-class Controller_Veiculo:
+class Controller_veiculo:
     def __init__(self):
         pass
         
     def inserir_veiculo(self) -> Veiculo:
         ''' Ref.: https://cx-oracle.readthedocs.io/en/latest/user_guide/plsql_execution.html#anonymous-pl-sql-blocks'''
         
-        # Cria uma nova conexão com o banco
-        oracle = OracleQueries()
-        # Recupera o cursos para executar um bloco PL/SQL anônimo
-        cursor = oracle.connect()
-        # Cria a variável de saída com o tipo especificado
-        output_value = cursor.var(int)
-
-        #Solicita ao usuario a nova descrição do produto
-        descricao_novo_veiculo = input("Descrição (Novo): ")
-
-        # Cria um dicionário para mapear as variáveis de entrada e saída
-        data = dict(codigo=output_value, descricao_produto=descricao_novo_produto)
-        # Executa o bloco PL/SQL anônimo para inserção do novo produto e recuperação da chave primária criada pela sequence
-        cursor.execute("""
-        begin
-            :codigo := PRODUTOS_CODIGO_PRODUTO_SEQ.NEXTVAL;
-            insert into produtos values(:codigo, :descricao_produto);
-        end;
-        """, data)
-        # Recupera o código do novo produto
-        codigo_produto = output_value.getvalue()
-        # Persiste (confirma) as alterações
-        oracle.conn.commit()
-        # Recupera os dados do novo produto criado transformando em um DataFrame
-        df_produto = oracle.sqlToDataFrame(f"select codigo_produto, descricao_produto from produtos where codigo_produto = {codigo_produto}")
-        # Cria um novo objeto Produto
-        novo_produto = Produto(df_produto.codigo_produto.values[0], df_produto.descricao_produto.values[0])
-        # Exibe os atributos do novo produto
-        print(novo_produto.to_string())
-        # Retorna o objeto novo_produto para utilização posterior, caso necessário
-        return novo_produto
-
-    def atualizar_produto(self) -> Produto:
         # Cria uma nova conexão com o banco que permite alteração
         oracle = OracleQueries(can_write=True)
         oracle.connect()
 
-        # Solicita ao usuário o código do produto a ser alterado
-        codigo_produto = int(input("Código do Produto que irá alterar: "))        
+        # Solicita ao usuario o novo id do veiculo 
+        idCarro = input("idCarro (Novo): ")
 
-        # Verifica se o produto existe na base de dados
-        if not self.verifica_existencia_produto(oracle, codigo_produto):
-            # Solicita a nova descrição do produto
-            nova_descricao_produto = input("Descrição (Novo): ")
-            # Atualiza a descrição do produto existente
-            oracle.write(f"update produtos set descricao_produto = '{nova_descricao_produto}' where codigo_produto = {codigo_produto}")
+        if self.verifica_existencia_idCarro(oracle, idCarro):
+            # Solicita ao usuario o novo modelo do veiculo
+            modelo = input("modelo (Novo): ")
+            # Solicita ao usuario a cor do veiculo
+            cor = input("cor (Novo): ")
+            # Solicita ao usuario o ano do veiculo
+            anoCarro = input("ano (Novo): ")
+            # Solicita ao usuario o chassi do veiculo
+            chassiCarro = input("chassi (Novo): ")
+            # Solicita ao usuario o tipo de cambio do veiculo
+            tipoCambio = input("tipo de cambio (Novo): ")
+            # Solicita ao usuario o fabricante do veiculo
+            fabricante = input("fabricante do carro (Novo): ")
+            # Insere e persiste o novo veiculo
+            oracle.write(f"insert into veiculos values ('{idCarro}', '{modelo}', '{modelo})', '{cor}', '{anoCarro}', '{chassiCarro}', '{tipoCambio}', '{fabricante}' ")
+            # Recupera os dados do novo veiculo criado transformando em um DataFrame
+            df_veiculo = oracle.sqlToDataFrame(f"select idCarro, modelo from veiculos where idCarro = '{idCarro}'")
+            # Cria um novo objeto Veiculo
+            novo_Veiculo = Veiculo(df_veiculo.idCarro.values[0], df_veiculo.modelo.values[0], df_veiculo.cor.values[0],
+                            df_veiculo.anoCarro.values[0], df_veiculo.chassiCarro.values[0], df_veiculo.tipoCambio.values[0], df_veiculo.fabricante.values[0])
+            # Exibe os atributos do novo veiculo
+            print(Veiculo.to_string())
+            # Retorna o objeto novo_veiculo para utilização posterior, caso necessário
+            return novo_Veiculo
+        else:
+            print(f"O veiculo {idCarro} já está cadastrado.")
+            return None
+
+    def atualizar_veiculo(self) -> Veiculo:
+        # Cria uma nova conexão com o banco que permite alteração
+        oracle = OracleQueries(can_write=True)
+        oracle.connect()
+
+        # Solicita ao usuário o código do veiculo a ser alterado
+        idCarro = int(input("Código do Veiulo que irá alterar: "))        
+
+        # Verifica se o veiculo existe na base de dados
+        if not self.verifica_existencia_veiculo(oracle, idCarro):
+            # Solicita o novo modelo do veiculo
+            novo_modelo = input("modelo (Novo): ")
+            # Atualiza o modelo do veiculo existente
+            oracle.write(f"update veiculos set modelo = '{novo_modelo}' where idCarro = {idCarro}")
+            # Solicita ao usuario a nova cor do veiculo
+            nova_cor = input("cor (Novo): ")
+            # Atualiza a cor do veiculo existente
+            oracle.write(f"update veiculos set cor = '{nova_cor}' where idCarro = {idCarro}")
+            # Solicita ao usuario o novo ano do veiculo
+            novo_ano = input("ano (Novo): ")
+            # Atualiza o ano do veiculo existente
+            oracle.write(f"update veiculos set anoCarro = '{novo_ano}' where cpf = {idCarro}")
+            # Solicita ao usuario o novo chassi do veiculo
+            novo_chassiCarro = input("chassi (Novo): ")
+            # Atualiza o chassi existente
+            oracle.write(f"update veiculos set chassiCarro = '{novo_chassiCarro}' where idCarro = {idCarro}")
+            # Solicita o novo cambio do veiculo
+            novo_tipoCambio = input("cambio (Novo): ")
+            # Atualiza o cambio do veiculo existente
+            oracle.write(f"update veiculos set cambio = '{novo_tipoCambio}' where idCarro = {idCarro}")
+            # Solicita o novo fabricante do veiculo
+            novo_fabricante = input("fabricante (Novo): ")
+            # Atualiza o fabricante do veiculo existente
+            oracle.write(f"update veiculo set fabricante = '{novo_fabricante}' where idCarro = {idCarro}")
             # Recupera os dados do novo produto criado transformando em um DataFrame
-            df_produto = oracle.sqlToDataFrame(f"select codigo_produto, descricao_produto from produtos where codigo_produto = {codigo_produto}")
+           ''' falta fazer essa parte do codigo: '''
+           
+            df_veiculo = oracle.sqlToDataFrame(f"select codigo_produto, descricao_produto from produtos where codigo_produto = {codigo_produto}")
             # Cria um novo objeto Produto
             produto_atualizado = Produto(df_produto.codigo_produto.values[0], df_produto.descricao_produto.values[0])
             # Exibe os atributos do novo produto
