9eb1878878b1c577c0f487b197159986c61ac5bf
Thiago Tresse <thiagotresse@hotmail.com>
2023-10-17 23:20:02 -0300

    CREATE TABLES FUNCIONANDO

--- sql/create_tables_pedidos.sql
+++ sql/create_tables_pedidos.sql
@@ -1,6 +1,6 @@
 /*Apaga os relacionamentos*/
-ALTER TABLE LABDATABASE.Cliente DROP CONSTRAINT Cliente_FK;
-ALTER TABLE LABDATABASE.Veiculo DROP CONSTRAINT Veiculo_FK;
+ALTER TABLE LABDATABASE.VendaVeiculo DROP CONSTRAINT Cliente_VendaVeiculo_fk;
+ALTER TABLE LABDATABASE.VendaVeiculo DROP CONSTRAINT Veiculo_VendaVeiculo_fk;
 
 /*Apaga as tabelas*/
 DROP TABLE LABDATABASE.Cliente;
@@ -8,64 +8,59 @@ DROP TABLE LABDATABASE.Veiculo;
 DROP TABLE LABDATABASE.VendaVeiculo;
 
 /*Apaga as sequences*/
-DROP SEQUENCE LABDATABASE.cpfCliente_SEQ;
-DROP SEQUENCE LABDATABASE.idCarro_SEQ;
-DROP SEQUENCE LABDATABASE.idVenda_SEQ;
+DROP SEQUENCE idCarro_SEQ;
+DROP SEQUENCE idVenda_SEQ;
+
+/*Cria as sequences*/
+CREATE SEQUENCE idCarro_SEQ;
+CREATE SEQUENCE idVenda_SEQ;
 
 /*Cria as tabelas*/
-CREATE TABLE Cliente (
-                cpfCliente VARCHAR(11) NOT NULL,
-                idCliente NUMERIC NOT NULL,
-                nome VARCHAR(50) NOT NULL,
-                email VARCHAR(50) NOT NULL,
-                telefone NUMERIC NOT NULL,
+CREATE TABLE LABDATABASE.Cliente (
+                cpfCliente VARCHAR(15) NOT NULL,
+                idCliente NUMERIC(10) NOT NULL,
+                nome VARCHAR(60) NOT NULL,
+                email VARCHAR(40) NOT NULL,
+                telefone NUMERIC(15) NOT NULL,
                 endereco VARCHAR(255) NOT NULL,
                 CONSTRAINT Cliente_pk PRIMARY KEY (cpfCliente)
 );
 
 
-CREATE TABLE Veiculo (
-                idCarro NUMERIC NOT NULL,
-                modelo VARCHAR(25) NOT NULL,
-                cor VARCHAR(25) NOT NULL,
+CREATE TABLE LABDATABASE.Veiculo (
+                idCarro NUMERIC DEFAULT idCarro_SEQ.NEXTVAL NOT NULL,
+                modelo VARCHAR(100) NOT NULL,
+                cor VARCHAR(100) NOT NULL,
                 anoCarro NUMERIC NOT NULL,
-                chassiCarro NUMERIC NOT NULL,
-                tipoCambio NUMERIC NOT NULL,
+                chassiCarro VARCHAR(18) NOT NULL,
+                tipoCambio VARCHAR(50) NOT NULL,
                 fabricante VARCHAR(25) NOT NULL,
                 CONSTRAINT Veiculo_pk PRIMARY KEY (idCarro)
 );
 
 
-CREATE TABLE VendaVeiculo (
-                idVenda NUMERIC NOT NULL,
+CREATE TABLE LABDATABASE.VendaVeiculo (
+                idVenda NUMERIC DEFAULT idVenda_SEQ.NEXTVAL NOT NULL,
                 valorVenda NUMERIC NOT NULL,
                 dataVenda DATE NOT NULL,
                 idVendedor NUMERIC NOT NULL,
                 idCarro NUMERIC NOT NULL,
-                cpfCliente VARCHAR(11) NOT NULL,
+                cpfCliente VARCHAR(15) NOT NULL,
                 CONSTRAINT VendaVeiculo_pk PRIMARY KEY (idVenda)
 );
 
 
-/*Cria as sequences*/
-CREATE SEQUENCE LABDATABASE.cpfCliente_SEQ;
-CREATE SEQUENCE LABDATABASE.idCarro_SEQ;
-CREATE SEQUENCE LABDATABASE.idVenda_SEQ;
-
 /*Cria os relacionamentos*/
-ALTER TABLE VendaVeiculo ADD CONSTRAINT Cliente_VendaVeiculo_fk
+ALTER TABLE LABDATABASE.VendaVeiculo ADD CONSTRAINT Cliente_VendaVeiculo_fk
 FOREIGN KEY (cpfCliente)
-REFERENCES LABDATABASE.Cliente (cpfCliente)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
+REFERENCES LABDATABASE.Cliente (cpfCliente);
 
-ALTER TABLE VendaVeiculo ADD CONSTRAINT Veiculo_VendaVeiculo_fk
+
+
+ALTER TABLE LABDATABASE.VendaVeiculo ADD CONSTRAINT Veiculo_VendaVeiculo_fk
 FOREIGN KEY (idCarro)
-REFERENCES LABDATABASE.Veiculo (idCarro)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
+REFERENCES LABDATABASE.Veiculo (idCarro);
+
 
 /*Garante acesso total as tabelas*/
 GRANT ALL ON LABDATABASE.Cliente TO LABDATABASE;
