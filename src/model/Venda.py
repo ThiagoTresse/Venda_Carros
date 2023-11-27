9ae6b2f9c76568567ce02a92262173ca7db11955
“labdatabase” <howardcruzroatti@gmail.com>
2023-11-27 14:58:59 -0300

    venda ajustada

--- src/model/Venda.py
+++ src/model/Venda.py
@@ -8,15 +8,15 @@ class VendaVeiculo:
                  valorVenda:float=None,
                  dataVenda:int=None,
                  idVendedor:int=None,
-                 Cliente:str=None,
-                 Veiculo:str=None
+                 cliente:Cliente=None,
+                 veiculo:Veiculo=None
                  ):
         self.set_idVenda(idVenda)
         self.set_valorVenda(valorVenda)
         self.set_dataVenda(dataVenda)
         self.set_idVendedor(idVendedor)
-        self.set_cliente(Cliente)
-        self.set_veiculos(Veiculo)
+        self.set_cliente(cliente)
+        self.set_veiculo(veiculo)
 #Getters
     def get_idVenda(self) -> int:
         return self.idVenda
@@ -33,8 +33,8 @@ class VendaVeiculo:
     def get_cliente(self) -> Cliente:
         return self.cliente
 
-    def get_veiculos(self) -> Veiculo:
-        return self.veiculos
+    def get_veiculo(self) -> Veiculo:
+        return self.veiculo
 
 #Setters
     def set_idVenda(self, idVenda: int):
@@ -49,12 +49,18 @@ class VendaVeiculo:
     def set_idVendedor(self, idVendedor: int):
         self.idVendedor = idVendedor
 
-    def set_cliente(self, cliente: Cliente):
+    def set_cliente(self, cliente:Cliente):
         self.cliente = cliente
 
-    def set_veiculos(self, veiculos: Veiculo):
-        self.veiculos = veiculos
+    def set_veiculo(self, veiculo: Veiculo):
+        self.veiculo = veiculo
 
 #ToString
     def to_string(self) -> str:
-        return (f"idVenda: {self.get_idVenda()} | valorVenda: {self.get_valorVenda()} | dataVenda: {self.get_dataVenda()} | idVendedor: {self.get_idVendedor()} | cpfCliente: {self.get_cliente().get_cpfCliente()} | Veiculo: {self.get_veiculos().get_idCarro()}")
+        return (f"idVenda: {self.get_idVenda()} | "
+                f"valorVenda: {self.get_valorVenda()} | "
+                f"dataVenda: {self.get_dataVenda()} | " 
+                f"idVendedor: {self.get_idVendedor()} | "
+                f"cliente: {self.get_cliente().get_cpfCliente()} | "
+                f"veiculo: {self.get_veiculo().get_idCarro()}"
+        )
